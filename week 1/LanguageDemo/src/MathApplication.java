30cebbaa7bc3d3e7c7313ae3139fecf0b94a173b
VVSteven <135930301+VVSteven@users.noreply.github.com>
2024-01-30 09:42:00 -0700

    completed Mod3 Exercise 2 - MathApp

--- week 1/LanguageDemo/src/MathApplication.java
+++ week 1/LanguageDemo/src/MathApplication.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MathApplication {
     public static void main(String[] args) {
         //Question 1:
@@ -13,13 +15,46 @@ public class MathApplication {
         System.out.println("The highest salary is " + highestSalary);
 
         System.out.println("Answer 2");
-
         int carPrice = 20000;
         int truckPrice = 30000;
 
         int cheapCar = Math.min(carPrice, truckPrice);
 
         System.out.println("The cheapest car is " + cheapCar);
+
+        System.out.println("Answer 3");
+        float circleRadius = 7.25f;
+        double areaCircle = Math.PI * circleRadius;
+
+        System.out.println("The area of a circle with a radius of " + circleRadius + " is " + areaCircle );
+
+        System.out.println("Answer 4");
+        double number4 = 25.0;
+        double squareRoot = Math.sqrt(number4);
+
+        System.out.println("Square root of " + number4 + " is " + squareRoot);
+
+        System.out.println("Answer 5");
+        int x1 = 5;
+        int x2 = 10;
+        int y1 = 85;
+        int y2 = 50;
+
+        double distance = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+
+        System.out.println("The distance between (" + x1 + "," + x2 + ") and (" + y1 + "," + y2 + ") is " + distance);
+
+        System.out.println("Answer 6");
+        double number6 = -3.8;
+        double absoluteValue = Math.abs(number6);
+
+        System.out.println("The absolute value of " + number6 + " is " + absoluteValue);
+
+        System.out.println("Answer 7");
+        Random randomGen = new Random();
+        int range = 2;
+        int randomNumber = randomGen.nextInt(range);
+        System.out.println("A random number between 0 and " + range + " is " + randomNumber);
 
     }
 }
