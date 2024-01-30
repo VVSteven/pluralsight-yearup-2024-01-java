de84f7ee78965fd7ad69fbc36f105001a393d169
VVSteven <135930301+VVSteven@users.noreply.github.com>
2024-01-29 19:28:27 -0700

    did 2 promblems

---
+++ week 1/FullNameGenerator/src/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] var0) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Your First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("============================");
+
+        System.out.print("Enter Your Middle Name: ");
+        String middleName = scanner.nextLine();
+        System.out.println("============================");
+
+        System.out.print("Enter Your Last Name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("============================");
+
+        System.out.print("Enter Your Suffix: ");
+        String suffix = scanner.nextLine();
+        System.out.println("============================");
+//    FRIEND INSPIRED
+        String fullName = firstName;
+
+        if (!middleName.isEmpty()){
+            fullName += " " + middleName;
+        }
+        fullName += " " + lastName;
+
+        if (!suffix.isEmpty()){
+            fullName += ", " + suffix;
+        }
+        System.out.println("Full Name: " + fullName);
+        System.out.println("============================");
+
+//        FIRST EDITION
+//        if (middleName.isEmpty() && suffix.isEmpty() ){
+//            System.out.println("Full Name: " + firstName + " " + lastName);
+//        } else if(middleName.isEmpty() || suffix.isEmpty()){
+//            if (middleName.isEmpty()){
+//                System.out.println("Full Name: " + firstName + " " + lastName + " " + suffix);
+//            }else {
+//                System.out.println("Full Name: " + firstName + " " + middleName + " " + lastName);
+//            }
+//        }else {
+//            System.out.println("Full Name: " + firstName + " " + middleName + " " + lastName + " " + suffix );
+//        }
+    }
+}
