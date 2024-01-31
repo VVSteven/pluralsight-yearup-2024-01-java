cd7caaff8baaae683c94e5ca8df7523d048097fb
VVSteven <135930301+VVSteven@users.noreply.github.com>
2024-01-31 09:09:27 -0700

    Updated Payroll Calculator with Overtime Pay

--- week 1/PayrollCalculator/src/PayrollCalculator.java
+++ week 1/PayrollCalculator/src/PayrollCalculator.java
@@ -16,11 +16,26 @@ public class PayrollCalculator {
         double payRate = Double.parseDouble(scanner.nextLine());
         System.out.println("============================");
 
-        double grossPay = payRate * hoursWorked;
+        System.out.print("Enter Your Regular Hours: ");
+        double regularHours = Double.parseDouble(scanner.nextLine());
+        System.out.println("============================");
+
+        double overtimePay = 0.0;
+
+        if (hoursWorked > regularHours) {
+            double overtime = hoursWorked - regularHours;
+            double overtimePayRate = payRate * 1.5;
+            overtimePay = overtime * overtimePayRate;
+            hoursWorked = hoursWorked - overtime;
+
+        }
+        double regularPay = hoursWorked * payRate;
+        double grossPay =  regularPay + overtimePay;
 
         System.out.println("Your Name is " + name);
         System.out.println("You Worked " + hoursWorked + " Hours");
-        System.out.println("Your Pay Rate is " +  payRate + " an Hour");
-        System.out.println("You Made " + grossPay + " Dollars");
+        System.out.println("Your Pay Rate is " + payRate + " an Hour");
+        System.out.println("You Made "+ regularPay + " Dollars and " + overtimePay + " Dollars in Overtime");
+        System.out.println("Your Gross Pay is " + grossPay + " Dollars");
     }
 }
