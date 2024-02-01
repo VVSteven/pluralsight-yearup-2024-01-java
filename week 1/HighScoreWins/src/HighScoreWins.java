fe96c4069f5fed861b7b8196366209081f1d2b3c
VVSteven <135930301+VVSteven@users.noreply.github.com>
2024-02-01 12:46:33 -0700

    saving

---
+++ week 1/HighScoreWins/src/HighScoreWins.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HighScoreWins {
+    public static void main(String[] var0){
+//        input home:visitor|10:9
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is The Team and Score Information? ");
+        String teamScoreInfo = scanner.nextLine();
+        scanner.close();
+
+        String[] teamScoreInfo = fullName.split(" ", -1);
+
+
+
+    }
+}
