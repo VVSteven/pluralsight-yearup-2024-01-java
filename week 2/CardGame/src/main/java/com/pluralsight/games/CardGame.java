fbdcc2cd37a3e4420f76bdcf9b13f35336dc1baf
VVSteven <135930301+VVSteven@users.noreply.github.com>
2024-02-07 12:26:07 -0700

    going to pull

--- week 2/CardGame/src/main/java/com/pluralsight/games/CardGame.java
+++ week 2/CardGame/src/main/java/com/pluralsight/games/CardGame.java
@@ -9,13 +9,27 @@ public class CardGame
     public void start()
     {
         // start by adding a few players
-        players.add(new Hand("Daisy"));
-        players.add(new Hand("Tee"));
-        players.add(new Hand("Gabe"));
+        players.add(new Hand("Elvis"));
+        players.add(new Hand("Jaquell"));
 
         // loop and deal the cards
+        System.out.println("Deal Round");
+        System.out.println("==================");
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        for (Hand player : players){
+            for (int i = 0; i < 2; i++){
+                Card card = deck.deal();
+                player.deal(card);
+            }
+            System.out.println(player.getPlayerName() + " has " + player.showHand());
+        }
 
         // loop and determine who won
+        for (int i = 0; i < players.size(); i++){
+        }
+
 
         // display the winner
     }
--- week 2/CardGame/src/main/java/com/pluralsight/games/Hand.java
+++ week 2/CardGame/src/main/java/com/pluralsight/games/Hand.java
@@ -2,50 +2,43 @@ package com.pluralsight.games;
 
 import java.util.ArrayList;
 
-public class Hand
-{
+public class Hand {
     private ArrayList<Card> cards = new ArrayList<>();
     private String playerName;
 
-    public Hand(String playerName)
-    {
+    public Hand(String playerName) {
         this.playerName = playerName;
     }
 
-    public String getPlayerName()
-    {
+    public String getPlayerName() {
         return playerName;
     }
 
-    public void deal(Card card)
-    {
+    public void deal(Card card) {
         cards.add(card);
     }
 
-    public int getSize()
-    {
+    public int getSize() {
         return cards.size();
     }
 
-    public int getValue()
-    {
+    public int getValue() {
         int value = 0;
 
-        for(Card card : cards)
-        {
+        for (Card card : cards) {
             value += card.getPointValue();
         }
 
         return value;
     }
 
-    public void showHand()
-    {
-        for(Card card: cards)
-        {
+    public String showHand() {
+        StringBuilder handString = new StringBuilder();
+        for (Card card : cards) {
             card.flip();
-            System.out.println(card + " ");
-            card.flip();
+            handString.append(card).append(" ");
+
         }
+        return handString.toString();
     }
 }
