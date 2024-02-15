7aa16dd69af1e6005fc2646f01d7d7e7d44ed734
VVSteven <135930301+VVSteven@users.noreply.github.com>
2024-02-15 10:07:47 -0700

    started car dealership project

---
+++ CarDealership/src/main/java/com/pluralsight/CarDealershipApplication.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CarDealershipApplication {
+    Scanner scanner = new Scanner(System.in);
+    
+}
---
+++ CarDealership/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        CarDealershipApplication app = new CarDealershipApplication();
+        app.run();
+    }
+}
